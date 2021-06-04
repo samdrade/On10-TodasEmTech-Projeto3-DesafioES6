dfa4c9940430b0aa6193d030002cc22b798f4756
Patrícia Barnabé <patbarnabe5@gmail.com>
2021-06-03 23:12:02 -0300

    Fase Extra: Adicionando a API dos Respositórios

--- entrega-projeto/patricia-barnabe/assets/js/app.js
+++ entrega-projeto/patricia-barnabe/assets/js/app.js
@@ -1,6 +1,8 @@
 const form = document.querySelector('form'); 
 const input = document.querySelector('.input');
 const profile = document.querySelector('.profile');
+const search_off = document.querySelector('.search_off')
+const home = document.querySelector('.home');
 
 form.addEventListener('submit', (event) => {
     event.preventDefault();
@@ -9,6 +11,7 @@ form.addEventListener('submit', (event) => {
 
     if(user) {
         getUser(user)
+        getRepos(user)
     } else {
         alert("Digite o usuário...")
     }
@@ -22,11 +25,16 @@ const getUser = async (user) => {
             throw new Error();
         }
         const userInfos = await requisicao.json();
+        home.remove();
         profile.innerHTML = createCard(userInfos);
-        //console.log(createCard(userInfos));
+        // console.log(createCard(userInfos));
+        // console.log(userInfos)
     } catch {
-        console.log("Usuário não encontrado");
+        profile.remove();
+        search_off.innerHTML = notFound();
     }
+
+    input.value = ''
 
 /* Alternativa usando o fetch:
     fetch(`https://api.github.com/users/${user}`)
@@ -43,14 +51,14 @@ const createCard = (user) => {
     </picture>
     <div>
         <h2 class="nome">${name}</h2>
-        <p>${login}</p>
-        <p>${bio}</p>
-        <ul class="">
-            <li>
+        <p class="login">@${login}</p>
+        <p class="bio">"${bio}"</p>
+        <ul class="ul">
+            <li class="li">
                 <span class="material-icons icons">people_outline</span>
                 <p>${followers}</p>
             </li>
-            <li>
+            <li class="li">
                 <span class="material-icons icons">class</span>
                 <p>${public_repos}</p>
             </li>
@@ -59,10 +67,23 @@ const createCard = (user) => {
     `
 }
 
+const notFound = () => {
+    return `
+        <div>
+            <h1 class="nome mensagem_erro"> Usuário não encontrado ☹ </h1>
+            <h3 class="mensagem"> Por favor, pesquise novamente </h3>
+        </div>
+        <picture>
+            <img class="imagem_notFound" src="../../../images/not-found.svg"
+        </picture>
+    `
+}
 
-
-
-
+const getRepos = (user) => {
+    fetch(`https://api.github.com/users/${user}/repos`)
+    .then(response => response.json())
+    .then(repos)
+}
 
 
 
