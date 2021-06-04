5c02cd214c5cc61556a5eeb15ff9483df2cb3ca6
marianadesouzaaguiar <mariana_desouza_2001@hotmail.com>
2021-06-04 17:49:15 -0300

    exercício semana 13

---
+++ entrega-projeto/mariana-aguiar/js/app.js
@@ -0,0 +1,65 @@
+const form = document.querySelector("form");
+const input = document.querySelector("#nomeDoUsuario");
+const profile = document.querySelector('.profile');
+
+
+form.addEventListener('submit', (event) => {
+   event.preventDefault();
+
+   const usuario = input.value.trim();
+
+   if(usuario){
+     return getUsuario(usuario)
+   } 
+    return alert('informe o usuário')
+})
+
+const getUsuario = async (usuario) => {
+
+   try {
+    const requisicao = await fetch(`https://api.github.com/users/${usuario}`)
+    if(requisicao.ok === false){
+        throw new Error();
+    }
+    const usuarioInformacoes = await requisicao.json();
+    profile.innerHTML = criarCardUsuario(usuarioInformacoes)
+   } catch {
+    profile.innerHTML = criarCardNotFound()
+   }
+    
+}
+criarCardNotFound = () => {
+    return `
+    <section class="errorMessage">
+        <h1 class="errorMessage__titulo">Usuário não encontrado :(</h1>
+        <p class="errorMessage__paragrafo">Pesquise novamente</p>
+        <img class="errorMessage__img" src="../../images/not-found.svg" alt="Imagem de erro">
+      </section>  
+`
+}
+
+const criarCardUsuario = (usuario) => {
+   const { login, name, bio, followers, public_repos, avatar_url} = usuario;
+    return `
+    <picture class="image__user">
+        <img class="user__image" src="${avatar_url}" alt="" />
+    </picture>
+    <div class="user__card">
+    <h2 class="user__name spacing">${name}</h2>
+    <p class="user__username paragraph spacing">${login}</p>
+    <p class="user__bio paragraph spacing">${bio}</p>
+    <ul class="badges spacing">
+    <li class="badge">
+       <span class="material-icons icon__profile">people_outline</span>
+       <p>${followers}</p>
+    </li>
+    <li class="badge">
+      <span class="material-icons icon__profile">class</span>
+      <p>${public_repos}</p>
+    </li>
+    </ul>
+    </div>
+   `
+}
+
+criarCardNotFound()
